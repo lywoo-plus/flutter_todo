4b9af002a1f92abd6da7f6c185e8fd18f2bae0b4
lywoo-plus <sreng.lywoo@gmail.com>
2025-12-21 00:18:04 +0700

    feat: Lottie

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/util.dart';
 import 'package:flutter_todo/views/widget_tree.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -12,20 +13,31 @@ class WelcomePage extends StatelessWidget {
         mainAxisAlignment: .center,
         spacing: 8,
         children: [
-          Text('Welcome', style: Theme.of(context).textTheme.headlineMedium),
-          Hero(
-            tag: 'logo',
-            child: Padding(
-              padding: .all(16.0),
-              child: ClipRRect(borderRadius: .circular(16), child: Image.asset('assets/images/nuxt.png')),
+          FittedBox(
+            child: Text(
+              'Welcome',
+              textAlign: .center,
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                letterSpacing: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.primaries.first,
+              ),
             ),
           ),
-          FilledButton.icon(
-            onPressed: () {
-              pushReplacement(context: context, page: WidgetTree());
-            },
-            label: Text('Login'),
-            icon: Icon(Icons.login),
+          Hero(
+            tag: 'logo',
+            child: Padding(padding: .all(16.0), child: Lottie.asset('assets/lotties/city_building.json')),
+          ),
+          Padding(
+            padding: .all(8.0),
+            child: FilledButton.icon(
+              onPressed: () {
+                pushReplacement(context: context, page: WidgetTree());
+              },
+              label: Text('Login'),
+              icon: Icon(Icons.login),
+              style: FilledButton.styleFrom(padding: .all(16), minimumSize: Size(double.maxFinite, 0)),
+            ),
           ),
         ],
       ),
