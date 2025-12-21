17da17cab37c1f992e91136f2f8b4698297ec225
lywoo-plus <sreng.lywoo@gmail.com>
2025-12-21 12:31:34 +0700

    feat: Stack and Positioned

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -9,37 +9,49 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: .center,
-        spacing: 8,
-        children: [
-          FittedBox(
-            child: Text(
-              'Welcome',
-              textAlign: .center,
-              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                letterSpacing: 50,
-                fontWeight: FontWeight.bold,
-                color: Colors.primaries.first,
+      body: Center(
+        child: Stack(
+          clipBehavior: .none,
+          alignment: .center,
+          children: [
+            Hero(
+              tag: 'logo',
+              child: Padding(padding: .all(16.0), child: Lottie.asset('assets/lotties/city_building.json')),
+            ),
+
+            FittedBox(
+              child: Text(
+                'Welcome'.toUpperCase(),
+                textAlign: .center,
+                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                  letterSpacing: 50,
+                  fontWeight: .w900,
+                  foreground: Paint()
+                    ..style = .stroke
+                    ..strokeWidth = 4
+                    ..color = Colors.black,
+                ),
               ),
             ),
-          ),
-          Hero(
-            tag: 'logo',
-            child: Padding(padding: .all(16.0), child: Lottie.asset('assets/lotties/city_building.json')),
-          ),
-          Padding(
-            padding: .all(8.0),
-            child: FilledButton.icon(
-              onPressed: () {
-                pushReplacement(context: context, page: WidgetTree());
-              },
-              label: Text('Login'),
-              icon: Icon(Icons.login),
-              style: FilledButton.styleFrom(padding: .all(16), minimumSize: Size(double.maxFinite, 0)),
+
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: .all(8.0),
+                child: FilledButton.icon(
+                  onPressed: () {
+                    pushReplacement(context: context, page: WidgetTree());
+                  },
+                  label: Text('Login'),
+                  icon: Icon(Icons.login),
+                  style: FilledButton.styleFrom(padding: .all(16), minimumSize: Size(.infinity, 0)),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
