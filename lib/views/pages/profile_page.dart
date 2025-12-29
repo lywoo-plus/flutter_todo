fbee4271d6ca075a5b87de06f0a770a40c0c1c0a
lywoo-plus <sreng.lywoo@gmail.com>
2025-12-29 23:30:18 +0700

    LayoutBuilder

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -5,6 +5,26 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile page'));
+    return Center(
+      child: Column(
+        spacing: 16,
+        children: [
+          CircleAvatar(radius: 60, backgroundImage: AssetImage('assets/images/nuxt.png')),
+          LayoutBuilder(
+            builder: (context, constraints) {
+              return FractionallySizedBox(
+                widthFactor: constraints.maxWidth > 100 ? 1 : 0.5,
+                child: Container(
+                  margin: .all(8),
+                  padding: .all(4),
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: Text('Lywoo'),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
